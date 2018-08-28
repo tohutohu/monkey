b287066da007db155356743aef641a499f102b90
とーふとふ <tohu.soy@gmail.com>
2018-08-28 17:47:54 +0900

    4.2

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -173,6 +173,10 @@ func TestErrorHandring(t *testing.T) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -277,4 +281,32 @@ func testNullObject(t *testing.T, obj object.Object) bool {
 		return false
 	}
 	return true
+}
+
+func TestStringLiteral(t *testing.T) {
+	input := `"Hello World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "HEllo World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatnation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -56,6 +56,9 @@ func TestNextToken(t *testing.T) {
 
 	10 == 10;
 	10 != 9;
+
+	"foobar"
+	"foo bar"
 	`
 
 	tests = []struct {
@@ -135,6 +138,8 @@ func TestNextToken(t *testing.T) {
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
 		{token.SEMICOLON, ";"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
 		{token.EOF, ""},
 	}
 	l = New(input)
